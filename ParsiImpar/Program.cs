71bd64b62ad9f84ec35ab000c14b086b8069d661
SabdruEzechiel <ezechiel.sandru@emanuel.ro>
2023-10-16 21:36:49 +0300

    a

---
+++ ParsiImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ParsiImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            Console.Write("Numar intreg= ");
+            x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Numarul este par");
+            }
+            else
+            {
+                Console.WriteLine("Numarul este impar");
+            }
+        }
+    }
+}
